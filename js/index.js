6bc388eed9609f4d66f637618c7e24ad5625a94f
ljf <285079883@qq.com>
2019-10-17 15:00:17 +0800

    index

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $('.main').mousemove(function(e) {
     var x = (e.pageX * -1 / 10), y = (e.pageY * -1 / 10);
     $(this).css('background-position', x + 'px ' + y + 'px');
-    var x2 = (e.pageX * -1 / 5) + 400, y2 = (e.pageY * -1 / 5) + 400;
-    $('#paperplane').css({'left': x2 + 'px','top':y2 + 'px'});
+    // var x2 = (e.pageX * -1 / 5) + 400, y2 = (e.pageY * -1 / 5) + 400;
+    // $('#widget').css({'left': x2 + 'px','top':y2 + 'px'});
 });
